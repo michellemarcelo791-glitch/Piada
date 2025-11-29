04bc491b022cdfaf35148ac1896fc4d969eddfb3
michelle <michellemarcelo791@gmail.com>
2025-11-28 21:34:21 -0300

    termino da atividade

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("folinha: porque me chamo folinha?");
+Console.WriteLine("- mãe: porque quando voce nasceu caiu uma folinha na sua cabeça");
+Console.WriteLine("Então gotinha fica curioso:");
+Console.WriteLine("- mamae porque meu nome e gotinha?");
+Console.WriteLine("- mãe: quando voce nasceu caiu gotinha na sua cabeça meu filho");
+Console.WriteLine("vendo isso tijolinho pergunta:");
+Console.WriteLine("- ahn ohm Thon??@!");
